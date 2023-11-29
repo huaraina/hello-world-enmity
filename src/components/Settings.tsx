52cd6f3a43253c4b0ba53e79adb79241c4ca31ad
huaraina <95729626+huaraina@users.noreply.github.com>
2023-11-29 06:49:43 -0800

    Update Settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,19 +1,76 @@
-import { FormRow, FormSwitch } from 'enmity/components';
+import { FormRow, FormSwitch, FormSection, ScrollView, View, Image, Text } from 'enmity/components';
 import { SettingsStore } from 'enmity/api/settings';
-import { React } from 'enmity/metro/common';
+import { React, Constants, Dialog, Navigation, StyleSheet } from 'enmity/metro/common';
 
 interface SettingsProps {
    settings: SettingsStore;
 }
 
 export default ({ settings }: SettingsProps) => {
-   return <FormRow
-      label='Example Setting'
-      trailing={
-         <FormSwitch
-            value={settings.get('example', true)}
-            onValueChange={() => settings.toggle('example', true)}
-         />
+   const styles = StyleSheet.createThemedStyleSheet({
+      container: {
+         flexDirection: "row",
+         justifyContent: "center",
+         alignItems: "center"
+      },
+      image: {
+         width: 70,
+         height: 70,
+         marginTop: 20,
+         marginLeft: 20
+      },
+      title: {
+         flexDirection: "column",
+      },
+      name: {
+         fontSize: 30,
+         paddingTop: 20,
+         paddingLeft: 20,
+         paddingRight: 30,
+         color: Constants.ThemeColorMap.HEADER_PRIMARY,
+      },
+      author: {
+         fontSize: 15,
+         paddingLeft: 20,
+         color: Constants.ThemeColorMap.HEADER_PRIMARY,
+      },
+      info: {
+         height: 45,
+         paddingTop: 3,
+         paddingBottom: 3,
+         justifyContent: "center",
+      },
+      footer: {
+         color: Constants.ThemeColorMap.HEADER_SECONDARY,
+         textAlign: 'center',
+         paddingTop: 10,
+         paddingBottom: 20
       }
-   />;
+   })
+   return (
+   <ScrollView>
+      <View style={styles.container}>
+         <Image
+            source={{uri: "https://avatars.githubusercontent.com/u/95729626"}}
+            style={styles.image}
+         />
+         <View style={styles.title}>
+            <Text style={styles.name}>Hello, World!</Text>
+            <Text style={styles.author}>by huaraina</Text>
+         </View>
+      </View>
+      <FormSection title="Press tha button!">
+         <FormRow
+            label="Press Me!"
+            styles={styles.info}
+            trailing={FormRow.Arrow}
+            leading={<FormRow.Icon source={{uri: "https://avatars.githubusercontent.com/u/95729626"}}/>}
+            onPress={() => {
+               Dialog.show({title: "you pressed it! :3", body: "Hello, World!", confirmText: "success, now leave."})
+            }}
+         >
+         </FormRow>
+      </FormSection>
+   </ScrollView>
+   );
 };
